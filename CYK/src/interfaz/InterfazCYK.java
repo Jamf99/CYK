f53d1ba3dd1d29f8f06867997270309b6e252b58
root <root@SirKal>
2019-04-12 14:54:01 -0500

    Actualizado

--- CYK/src/interfaz/InterfazCYK.java
+++ CYK/src/interfaz/InterfazCYK.java
@@ -1,16 +1,25 @@
 package interfaz;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
 
-public class InterfazCYK extends JFrame {
+public class InterfazCYK extends JFrame implements ActionListener {
+	
+	public final static String EMPEZAR = "Empezar";
+	
 	private JLabel labAlfabeto;
 	private JTextField txtAlfabeto;
 	private JLabel labCantidadVariables;
@@ -18,24 +27,48 @@ public class InterfazCYK extends JFrame {
 	private JButton butEmpezar;
 	
 	public InterfazCYK() {
-		setSize(new Dimension(500,300));
+		setSize(new Dimension(500,250));
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setTitle("Algoritmo de CYK");
 		setLayout(new BorderLayout());
-		labAlfabeto = new JLabel("<html>Ingrese el alfabeto de la gramática<br>d(separado por comas y sin espacios</html>");
+		labAlfabeto = new JLabel("<html>Ingrese el alfabeto de la gramática<br>(separado por comas y sin espacios)</html>");
 		txtAlfabeto = new JTextField();
 		labCantidadVariables = new JLabel("<html>Ingrese el número de variables<br>que tendrá la gramática</html>");
 		txtCantidadVariables = new JTextField();
 		butEmpezar = new JButton("Empezar");
+		butEmpezar.setActionCommand(EMPEZAR);
+		butEmpezar.addActionListener(this);
 		JPanel aux = new JPanel();
-		aux.setLayout(new GridLayout(2,2));
+		TitledBorder border = BorderFactory.createTitledBorder("Rellene todos los campos");
+		border.setTitleColor(Color.RED);
+		aux.setBorder(border);
+		aux.setLayout(new GridLayout(5,2));
+		aux.add(new JLabel());aux.add(new JLabel());
 		aux.add(labAlfabeto);
 		aux.add(txtAlfabeto);
+		aux.add(new JLabel());aux.add(new JLabel());
 		aux.add(labCantidadVariables);
 		aux.add(txtCantidadVariables);
+		aux.add(new JLabel());aux.add(new JLabel());
 		add(aux, BorderLayout.CENTER);
 		add(butEmpezar, BorderLayout.SOUTH);
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}catch(Exception e){	
+		}
+	}
+	
+	public void validar() {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent evento) {
+		String comando = evento.getActionCommand();
+		if(comando.equals(EMPEZAR)) {
+			validar();
+		}
 	}
 	
 	public static void main(String[] args) {
