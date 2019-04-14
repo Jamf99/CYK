ea7fecc60b46add127b6292adb08f95b924de1bc
root <root@SirKal>
2019-04-13 19:40:24 -0500

    Excepciones
    Sirven para validar el alfabeto y que los campos estén completos

---
+++ CYK/src/excepciones/AlfabetoInvalidoException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class AlfabetoInvalidoException extends Exception {
+
+	public AlfabetoInvalidoException() {
+		super("El alfabeto es inválido");
+	}
+	
+}
---
+++ CYK/src/excepciones/CampoVacioException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class CampoVacioException extends Exception {
+	
+	public CampoVacioException() {
+		super("Llene todos los campos");
+	}
+
+}
