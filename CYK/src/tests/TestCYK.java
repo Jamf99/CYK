dc92f5893ca218a9f2ec9cfe6c16fd760d91c84c
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-04-17 18:31:08 -0500

    Actualizado el Test del algoritmo CYK

--- CYK/src/tests/TestCYK.java
+++ CYK/src/tests/TestCYK.java
@@ -12,18 +12,21 @@ class TestCYK extends TestCase {
 	private void escenario1() {
 		String[] alfabeto = new String[2];
 		alfabeto[0] = "a"; alfabeto[1] = "b";
-		mundo = new Gramatica(2, alfabeto);
-		mundo.agregarProducciones(0, "AA");
-		mundo.agregarProducciones(0, "a");
-		mundo.agregarProducciones(1, "aA");
+		mundo = new Gramatica(4, alfabeto);
+		mundo.agregarProducciones(0, "BA");
+		mundo.agregarProducciones(0, "AC");
+		mundo.agregarProducciones(1, "CC");
 		mundo.agregarProducciones(1, "b");
-		mundo.agregarProducciones(1, "a");
+		mundo.agregarProducciones(2, "AB");
+		mundo.agregarProducciones(2, "a");
+		mundo.agregarProducciones(3, "BA");
+		mundo.agregarProducciones(3, "a");
 	}
 	
 	@Test
 	public void cykTest1() {
 		escenario1();
-		assertTrue(mundo.CYK("aba"));
+		assertTrue(mundo.CYK("bbab"));
 	}
 
 }
